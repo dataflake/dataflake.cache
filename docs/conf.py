d9c32c78d0a5c02329eab8e6c3d9b7eca53316c9
Jens Vagelpohl <jens@dataflake.org>
2009-11-30 09:54:25 +0000

    - copyright update
    git-svn-id: file:///svn-public/dataflake.cache/trunk@1826 835909ba-7c00-0410-bfa4-884f43845301

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'dataflake.cache'
-copyright = '2008, Jens Vagelpohl'
+copyright = '2008-2009, Jens Vagelpohl'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
