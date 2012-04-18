d69a4476dd25c2c2fa3c3d23107243700f2465c8
Jens Vagelpohl <jens@dataflake.org>
2012-04-18 10:50:12 +0200

    - remove decorator implementation workaround, not needed after   dropping support for Python 2.4 and 2.5

--- dataflake/cache/utils.py
+++ dataflake/cache/utils.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2009-2010 Jens Vagelpohl and Contributors. All Rights Reserved.
+# Copyright (c) 2009-2012 Jens Vagelpohl and Contributors. All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
@@ -15,30 +15,7 @@
 $Id$
 """
 
-try:
-    from functools import wraps
-except ImportError:
-    ASSIGN = ('__module__', '__name__', '__doc__')
-    UPDATE = ('__dict__',)
-
-    def wrap(_wrapped_func, *args, **kw):
-        def _wrapped(*moreargs, **morekw):
-            return _wrapped_func(*(args+moreargs), **dict(kw, **morekw))
-        return _wrapped
-
-    def update_wrapper(wrapper, wrapped, assigned=ASSIGN, updated=UPDATE):
-        for attr in assigned:
-            setattr(wrapper, attr, getattr(wrapped, attr))
-        for attr in updated:
-            getattr(wrapper, attr).update(getattr(wrapped, attr))
-        return wrapper
-
-    def wraps(wrapped, assigned=ASSIGN, updated=UPDATE):
-        return wrap( update_wrapper
-                   , wrapped=wrapped
-                   , assigned=assigned
-                   , updated=updated
-                   )
+from functools import wraps
 
 def protect_with_lock(decorated):
     """ Decorator function: serialize access to 'decorated' using a lock
