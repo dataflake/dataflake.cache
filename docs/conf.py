474e2af3b1f87921a346164411db097a0b292779
Jens Vagelpohl <jens@netz.ooo>
2017-05-21 11:35:40 +0200

    - rename Sphinx folders

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = [ 'sphinx.ext.autodoc'
              ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -130,7 +130,7 @@ html_favicon = 'dataflake.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
